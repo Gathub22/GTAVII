16232b943fc566153b0b83d3e2cf2d6570f441c4
Gathub22 <gatomail22@proton.me>
2025-05-16 12:24:39 +0200

    Uploaded character jumping

--- CharacterController.cs
+++ CharacterController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 class CharacterController : MonoBehaviour
 {
 
+	public float JumpStrength;
+	private Rigidbody rb;
+
 	void Start()
 	{
-
+		rb = GetComponent<Rigidbody>();
 	}
 
 	void Update()
 	{
+
+		if (Input.GetKey(Input.Spacebar))
+		{
+			rb.AddForce(Vector3.up * JumpStrength * Time.deltaTime);
+		}
 
 	}
 
